e35e77d7dc09bee0010f9580b6b7c7ff48128ef7
HarmonyClexx <harmonymupindu@gmail.com>
2023-02-17 13:50:50 -0800

    committing 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 	char alphabet;
-	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++) {
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
 		putchar(alphabet);
        	}
 	putchar('\n');
