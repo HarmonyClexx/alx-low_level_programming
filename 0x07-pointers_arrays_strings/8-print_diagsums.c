4aa26ac24b8508be8bc1281ee4b62e45f99a6da9
HarmonyClexx <harmonymupindu@gmail.com>
2023-03-06 13:24:38 -0800

    adding print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1 = 0, sum2 = 0;
-	for (int i = 0; i < size; i++)
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < (size * size); i++)
 	{
-		// add the ith element of the ith row to the first sum
-		sum1 += a[i * size + i];
-		// add the ith element of the (size-i-1)th row to the second sum
-		sum2 += a[(size - i - 1) * size + i];
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
 	}
-	printf("Diagonal sum 1: %d\n", sum1);
-	printf("Diagonal sum 2: %d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
