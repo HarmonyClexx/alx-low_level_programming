d78aa751214073a10c5f6b2775992e4f57dbac65
HarmonyClexx <harmonymupindu@gmail.com>
2023-03-06 12:59:32 -0800

    adding strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,13 +7,17 @@
  * Return: a pointer to the byte in s that matches one of the
  * bytes in accept, or NULL if no such byte is found.
  */
-char *_strpbrk(char *s, char *accept) {
-    for (; *s != '\0'; s++) {
-        for (char *a = accept; *a != '\0'; a++) {
-            if (*s == *a) {
-                return (s);
-            }
-        }
-    }
-    return ('\0');
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
 }
