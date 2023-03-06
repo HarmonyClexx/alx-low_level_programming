0ecf97da855be6f2ff83502619d430832ab441b0
HarmonyClexx <harmonymupindu@gmail.com>
2023-03-06 13:21:50 -0800

    adding print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,16 @@
  * @size: size of the matrix
  * Return: no return.
  */
-void print_diagsums(int *a, int size) {
-    int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < size; i++) {
-        // add the ith element of the ith row to the first sum
-        sum1 += a[i*size + i];
-        // add the ith element of the (size-i-1)th row to the second sum
-        sum2 += a[(size-i-1)*size + i];
-    }
-    printf("Diagonal sum 1: %d\n", sum1);
-    printf("Diagonal sum 2: %d\n", sum2);
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < size; i++)
+	{
+		// add the ith element of the ith row to the first sum
+		sum1 += a[i * size + i];
+		// add the ith element of the (size-i-1)th row to the second sum
+		sum2 += a[(size - i - 1) * size + i];
+	}
+	printf("Diagonal sum 1: %d\n", sum1);
+	printf("Diagonal sum 2: %d\n", sum2);
 }
