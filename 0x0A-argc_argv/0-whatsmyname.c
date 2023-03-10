f4c51baa838fccc2e415a34e7f026496defc0e22
HarmonyClexx <harmonymupindu@gmail.com>
2023-03-10 03:08:41 -0800

    adding first files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
 /**
  * main - prints its name, followed by a new line.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	char *program_name = strrchr(argv[0], '/');
-	if (program_name == NULL)
-	{
-		program_name = argv[0];
-	}
-	else
-	{
-		program_name++;
-	}
-	printf("%s\n", program_name);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
